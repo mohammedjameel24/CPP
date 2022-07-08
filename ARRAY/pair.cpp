cb3a21d9e9300f1d241cede9a3e96c94bf4ce981
Mohd Jamil <mohammedjameel2412@gmail.com>
2022-07-08 10:37:33 +0000

    Pairs all approaches ✅

--- ARRAY/pair.cpp
+++ ARRAY/pair.cpp
@@ -10,32 +10,81 @@ OUTPUT
 [10,-6]
 */
 
-//Brute force Approach
-
 #include <iostream>
 #include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-vector<int> pairSum(vector<int> arr , int S)
+//Brute force Approach
+
+vector<int> pairSumBrute(vector<int> arr , int S)
 {
     //First traverse through the vector array
     for(int i = 0; i < arr.size(); i++){
         for(int j = i + 1; j < arr.size(); j++){
             if(arr[i] + arr[j] == S){
-                cout << arr[i] << "," << arr[j];
+                cout << arr[i] << "," << arr[j] << endl;
             }
         }            
 	}
     return {};
 }
 
+//Sorted Array Approach
+vector<int> pairSumSort(vector<int> arr , int S)
+{
+    //sort the array
+    sort(arr.begin(),arr.end());
+    // traverse through the vector array
+    for(int i = 0; i < arr.size(); i++){
+        for(int j = i + 1; j < arr.size(); j++){
+            if(arr[i] + arr[j] == S){
+                cout << arr[i] << "," << arr[j] << endl;
+            }
+        }            
+	}
+    return {};
+}
+
+// Data structure Approach
+
+vector<int> pairSumDS(vector<int> arr, int S)
+{
+    unordered_set<int> set;
+    vector<int> result;
+
+    for(int i = 0; i < arr.size(); i++)
+    {
+        int x = S - arr[i];
+        if(set.find(x) != set.end())
+        {
+            result.push_back(x);
+            result.push_back(arr[i]);
+            return result;
+        }
+        set.insert(arr[i]);
+        
+    }
+    return {};
+}
+
+
 int main(){
 
 	vector<int> arr{10, 5, 2 , 3, -6, 9 , 11};
 	int S = 4;
     
-    pairSum(arr,S);
+    pairSumBrute(arr,S); // O(N²)
+    pairSumSort(arr,S); // O(Nlog N)
+    auto p = pairSumDS(arr,S); // O(N)
+    if(p.size() == 0)
+    {
+        cout << "no such pair" << endl;
+    }
+    else{
+        cout<<p[0] << "," <<p[1] << endl;
+    }
 
 	return 0;
 }
