73a18c6b7a7288458257d78e381315717d263117
Mohd Jamil <mohammedjameel2412@gmail.com>
2022-07-20 06:11:10 +0000

    done longest band✅

--- ARRAY/longest_band.cpp
+++ ARRAY/longest_band.cpp
@@ -14,14 +14,15 @@ Output
 #include <algorithm>
 
 using namespace std;
-
+//----------------------Sorting Approach----------------------//
 int longest_band_sort(vector<int> arr)
 {
-    int count = 0;
+    
     int n = arr.size();
     sort(arr.begin(),arr.end());
-    // sorted arr{0,1,2,3,4,5,6,7,9,10,12,18}
+    // sorted arr{0,1,2,3,5,6,7,9,10,12,18}
 
+    int count = 0;
     for(int i = 0 ; i <=n; i++)
     {
         if(arr[i+1] - arr[i] == 1)
@@ -32,10 +33,41 @@ int longest_band_sort(vector<int> arr)
     return count;
 }
 
+int longest_band(vector<int> arr)
+{
+    int n = arr.size();
+    unordered_set<int> s;
+
+    int largestLen = 1;
+    //insert into unordered set
+    for(int x : arr)
+    {
+        s.insert(x);
+    }
+
+    for(auto element : s){
+        int parent = element - 1;
+        if(s.find(parent) == s.end())
+        {
+            int next_no = element + 1;
+            int count = 1;
+            while(s.find(next_no)!=s.end()){
+                next_no++;
+                count++;
+            }
+            if(count>largestLen){
+                largestLen = count;
+            }
+        }
+    }
+    return largestLen;
+}
+
 int main()
 {
-    vector<int> arr{1,9,3,0,18,5,2,4,10,7,12,6};
+    vector<int> arr{1,9,3,5,2,4,10,12,6};
 
     cout << longest_band_sort(arr) << endl; //O(N log)
+    cout << longest_band(arr) << endl; //O(N)
     
 }
